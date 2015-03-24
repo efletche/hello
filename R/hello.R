ddb6056671b20ddfbcba4ce05eb05bb880859433
Eric Fletcher <fletcher.e@gmail.com>
2015-03-23 21:01:08 -1000

    First Edit

--- R/hello.R
+++ R/hello.R
@@ -2,5 +2,9 @@
 #' @name hello
 #' @title hello: A minimal test R-Package 
 #' 
-#' @description The 'hello' package is desigined to as to   
+#' @description The 'hello' package is desigined as an minimal example to test
+#' out if issues with the R-package creation tools.
+#' 
+#' @docType package
+#' 
 NULL
